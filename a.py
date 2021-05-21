e20b13dfb61629fe7190eb7bc48c1fc43729a4a9
ramishtaha <trixterthe@gmail.com>
2021-05-21 18:31:34 +0530

    first commit

---
+++ a.py
@@ -0,0 +1,12 @@
+import time
+import os
+while(True):
+    os.system("cls")
+    print("/")
+    time.sleep(0.2)
+    os.system("cls")
+    print("-")
+    time.sleep(0.2)
+    os.system("cls")
+    print("\\")
+    time.sleep(0.2)
